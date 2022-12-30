e43c13cc21a1d37f2cf3b8b3d89c343186b8a6d2
ZedPriest <Charleschukwuemeka47@gmail.com>
2022-12-30 17:27:39 +0100

    Add Necessary Comments

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,4 @@
+//Nav Bar
 const btn = document.getElementById("menu-btn");
 const nav = document.getElementById("menu");
 
@@ -6,13 +7,13 @@ btn.addEventListener("click", () => [
   nav.classList.toggle("flex"),
   nav.classList.toggle("hidden"),
 ]);
-
+//For The Animation
 const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if(entry.isIntersecting){
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
       entry.target.classList.add("unhide");
     } else {
-      entry.target.classList.remove("unhide")
+      entry.target.classList.remove("unhide");
     }
   });
 });
